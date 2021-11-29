bed02aadb5e42f98af1a7e38a061eea08fa5e484
Francesco Nisi <francesconisi95@gmail.com>
2021-11-29 20:25:09 +0100

    fog

--- src/script.js
+++ src/script.js
@@ -2,15 +2,21 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { SphereBufferGeometry } from "three";
-
+import dat from "dat.gui";
 /*Scene*/
 const scene = new THREE.Scene();
 /*Axis Helper*/
 const axisHelper = new THREE.AxisHelper();
 scene.add(axisHelper);
+/*DatGui*/
+const gui = new dat.GUI();
 /*Canvas*/
 const canvas = document.querySelector("canvas.webgl");
-
+/*Fog*/
+const fog = new THREE.Fog("#262837", 1, 15);
+gui.add(fog, "near").min(1).max(100).step(1);
+gui.add(fog, "far").min(1).max(100).step(1);
+scene.fog = fog;
 /*Meshes*/
 /*Plane*/
 const plane = new THREE.Mesh(
@@ -67,6 +73,25 @@ const bush3 = new THREE.Mesh(
 );
 bush3.position.set(1.7, 0.2, 2.5);
 scene.add(bush3);
+
+/*Graves*/
+const graves = new THREE.Group();
+scene.add(graves);
+const graveGeometry = new THREE.BoxBufferGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" });
+/*50 Graves*/
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 4 + Math.random() * 6;
+  const z = Math.cos(angle) * radius;
+  const x = Math.sin(angle) * radius;
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  grave.position.set(x, 0.2, z);
+  grave.rotation.z = Math.random() * 0.2;
+  grave.rotation.y = Math.random() * 0.2;
+
+  graves.add(grave);
+}
 /*Sizes*/
 const sizes = {
   width: window.innerWidth,
@@ -80,9 +105,9 @@ camera.position.y = 1;
 scene.add(camera);
 
 /*Lights*/
-const ambientLight = new THREE.AmbientLight("white", 0.5);
+const ambientLight = new THREE.AmbientLight("#b9d5ff", 0.12);
 scene.add(ambientLight);
-const directionalLight = new THREE.DirectionalLight("white", 1);
+const directionalLight = new THREE.DirectionalLight("#b9d5ff", 0.12);
 directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
 const directionalLightHelper = new THREE.DirectionalLightHelper(
@@ -90,6 +115,9 @@ const directionalLightHelper = new THREE.DirectionalLightHelper(
 );
 scene.add(directionalLightHelper);
 directionalLightHelper.visible = false;
+const pointLight = new THREE.PointLight("#ff7d46", 1, 7);
+pointLight.position.set(0, 1.8 / 2, 2);
+house.add(pointLight);
 
 /*Controls*/
 const controls = new OrbitControls(camera, canvas);
@@ -108,6 +136,7 @@ window.addEventListener("resize", () => {
 /*Renderer*/
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setClearColor("#262837");
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.render(scene, camera);
 
