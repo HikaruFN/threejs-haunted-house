a6225054a8a88d22776c023370e7263e6c366926
Francesco Nisi <francesconisi95@gmail.com>
2021-11-29 16:51:58 +0100

    push

---
+++ src/script.js
@@ -0,0 +1,13 @@
+import "./style.css";
+import * as THREE from "three";
+
+/*Scene*/
+const scene = new THREE.Scene();
+
+/*Meshes*/
+
+/*Sizes*/
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
